b0f27da69ae89285593a459e282afa773d5b379d
stradiuarius <mailforgames19847@gmail.com>
2020-07-12 13:21:27 +0300

    init rm n_m

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
+
+gulp.task('scss', function(){
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function(){
+  return gulp.src('src/**/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    }
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/sass/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('src/**/*.html', gulp.parallel('html'));
+  gulp.watch('src/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'))
